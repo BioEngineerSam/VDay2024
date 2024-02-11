459e270f55666c37d5293b80d1ae0347c7142687
Samantha Zammit <samanthazammit98@hotmail.com>
2024-02-11 13:27:33 -0500

    Add no button and new text

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.heart-text').click(function(){
+      $('.container').addClass('open');
+      $('.small-button').hide();
+    });
+
+    $('.above-heart-text').click(function(){
+        $('.container').addClass('open');
+        $('.small-button').hide();
+      });
+    
+    
+    $('.close').click(function(){
+      $('.container').removeClass('open');
+    });
+
+    // Function to display the popup
+    function displayPopup() {
+        document.getElementById('popup-container').style.display = 'block';
+    }
+    
+    // Function to close the popup
+    function closePopup() {
+        document.getElementById('popup-container').style.display = 'none';
+    }
+    
+    // Event listener for the button click
+    document.querySelector('.small-button').addEventListener('click', function() {
+        displayPopup();
+    });
+
+    document.querySelector('.close-popup').addEventListener('click', function() {
+        closePopup();
+    });
+  
+  });
